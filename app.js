186946fb301e075e1d84ee9763e918e9adf1208a
Shintaro Nakajima <nakamajin52@gmail.com>
2018-03-10 12:15:40 +0900

    canvasでフルスクリーンでvideo

--- app.js
+++ app.js
@@ -3,5 +3,3 @@ import {MDCRipple} from '@material/ripple';
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
 import {MDCTextField} from '@material/textfield';
 mdc.textField.MDCTextField.attachTo(document.querySelector('.my-input'));
-// document.querySelector('.mdc-text-field').MDCTextField.disabled = true;
-// document.querySelector('.mdc-text-field').MDCTextField.ripple = ripple;
